42c18d96107e0d32c8ae78d4d722ae9ff7e4146a
Joshua Richard Smith <veryjoshua@gmail.com>
2018-07-28 12:49:07 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const login =  () => {
+    if(true){
+
+    }else{
+
+    }
+    alert('You are logged in!!!')
+}
+
+// document.getElementById("login-form").addEventListener("click", function(event){
+//     event.preventDefault()
+// });
